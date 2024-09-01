b814e3ffdda20e3b4675fb6237ae8a348baefad3
shaoairai <shaoairai@gmail.com>
2024-09-01 17:42:53 +0800

    編譯路徑調整

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/memorize_words/',
+  base: '/memorize_words/',
   plugins: [vue()],
   resolve: {
     alias: {
